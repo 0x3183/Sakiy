4c8681d7e2ad118534e4a8d22d1095609875dc44
0x3183 <depecksti@gmail.com>
2022-10-12 22:34:51 +0200

    test update

--- Sakiy/Properties/AssemblyInfo.cs
+++ Sakiy/Properties/AssemblyInfo.cs
@@ -6,4 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Sakiy")]
 [assembly: ComVisible(false)]
 [assembly: Guid("85b8c6bb-78eb-4243-b1e0-e7cda55326bc")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+//test update
